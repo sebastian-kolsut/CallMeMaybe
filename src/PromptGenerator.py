1d81b25a3a4586db799fd968c4da95968df5b9a4
Sebastian Kolsut <skolsut@c3r13s1.42warsaw.pl>
2026-03-28 18:36:24 +0100

    all

---
+++ src/PromptGenerator.py
@@ -0,0 +1,80 @@
+from typing import Dict, Tuple
+import json
+
+
+class PromptGenerator:
+    def __init__(self, file_name: str) -> None:
+        with open(file_name, "r") as file:
+            self.data = json.load(file)
+
+        self.prompt = ""
+
+    def add_user_prompt(self, prompt: str) -> str:
+        self.prompt += (f"<|im_start|>user\n{prompt}\n<|im_end|>" +
+                        "\n<|im_start|>assistant\n<think>\n</think>\n")
+
+        return self.prompt
+
+    @staticmethod
+    def _get_parameters(params: Dict[str, Dict[str, str]]) -> str:
+        params_str = ""
+
+        for name, param in params.items():
+            type = param['type']
+            params_str += f"\t\t- {name}: {type}\n"
+
+        return params_str
+
+    def get_functions_data_to_str(self):
+        self.prompt = \
+            """
+            <|im_start|>system
+            You are a function calling LLM and your task is to pick
+            ONLY the function based on the user prompt.
+            (Write only the function name)
+
+            Those are the functions that you can pick:
+            """
+
+        data_str = ""
+        for function in self.data:
+            data_str += f"Function name: {function['name']}\n"
+            data_str += f"\t1. Description {function['description']}\n"
+            data_str += "\t2. Parameters\n"
+            data_str += self._get_parameters(function['parameters'])
+            data_str += \
+                f"\t3. Return:\n\t\t- type: {function['returns']['type']}\n"
+
+        data_str += "<|im_end|>\n"
+        self.prompt += data_str
+        return self.prompt
+
+    def get_function_data_from_name(self, name: str) -> Tuple[str, dict]:
+        self.prompt = \
+            """
+            <|im_start|>system
+            You are a function calling LLM and your task is to pick
+            the arguments from the user's prompt wich will be used
+            in the given function. If you finish the given argument write
+            a newline.The output has to be a valid json where the key is
+            the name of the parameter and the value is the value.
+
+            This is the function mentioned above:
+            """
+
+        data_str = ""
+
+        for function in self.data:
+            if function['name'] == name:
+                data_str += f"Function name: {function['name']}\n"
+                data_str += f"\t1. Description {function['description']}\n"
+                data_str += "\t2. Parameters\n"
+                data_str += self._get_parameters(function['parameters'])
+                data_str += \
+                    ("\t3. Return:\n\t\t- type: " +
+                     f"{function['returns']['type']}\n")
+                func_data = function
+                break
+
+        self.prompt = self.prompt + data_str + "<|im_end|>\n"
+        return self.prompt, func_data
---
+++ src/Prompter.py
@@ -0,0 +1,104 @@
+from llm_sdk.llm_sdk import Small_LLM_Model
+from src.PromptGenerator import PromptGenerator
+import numpy as np
+
+
+class Prompter:
+    def __init__(self, model: Small_LLM_Model, gen: PromptGenerator):
+        self.model = model
+        self.gen = gen
+
+    def _get_function_names_tokens(self):
+        tokenized_func_names = []
+
+        for func in self.gen.data:
+            tokenized_func_names.append(
+                self.model.encode(func['name'])[0].tolist())
+
+        return tokenized_func_names
+
+    @staticmethod
+    def _get_i_tokens(i: int, tokenized_func_names, found_name, eos_token):
+        tokens = []
+
+        for name in tokenized_func_names:
+            if i < len(name) and found_name == name[:i]:
+                tokens.append(name[i])
+
+        if not tokens:
+            return [eos_token]
+
+        return tokens
+
+    @staticmethod
+    def _apply_mask_to_logits(tokens, logits):
+        logits_copy = np.array(logits, dtype=float)
+        masked = np.full(logits_copy.shape, -np.inf)
+
+        masked[tokens] = logits_copy[tokens]
+        masked = masked.tolist()
+
+        return masked
+
+    def get_fn_name_from_llm(self, encoded):
+        eos_token = self.model.encode("<|im_end|>")[0].tolist()[0]
+        tokenized_func_names = self._get_function_names_tokens()
+        found_name = []
+
+        i = 0
+        while True:
+            logits = self.model.get_logits_from_input_ids(encoded)
+            tokens = self._get_i_tokens(
+                i, tokenized_func_names, found_name, eos_token)
+            logits = self._apply_mask_to_logits(tokens, logits)
+            next_word = int(np.argmax(logits))
+            encoded.append(next_word)
+            found_name.append(next_word)
+            if next_word == eos_token:
+                break
+            i += 1
+
+        return self.model.decode(found_name)
+
+    def _get_number_mask(self):
+        base_pieces = [
+            " -", "0", "1", "2", "3", "4", "5", "6",
+            "7", "8", "9", ".", "\n", "-", " "
+        ]
+
+        candidate_pieces = base_pieces
+        encoded_digits = []
+
+        for piece in candidate_pieces:
+            piece_tokens = self.model.encode(piece)[0].tolist()
+            encoded_digits.extend(piece_tokens)
+
+        return list(encoded_digits)
+
+    def _get_argument_from_llm(self, type: str, prompt: str):
+        encoded = self.model.encode(prompt)[0].tolist()
+        new_line = self.model.encode("\n")[0].tolist()[0]
+        if type == "number":
+            tokens = self._get_number_mask()
+
+        for _ in range(20):
+            logits = self.model.get_logits_from_input_ids(encoded)
+            if type == "number":
+                logits = self._apply_mask_to_logits(tokens, logits)
+            next_word = int(np.argmax(logits))
+            if next_word == new_line:
+                break
+            encoded.append(next_word)
+
+        return encoded
+
+    def get_args_to_function(self, func_data: dict, prompt: str):
+        prompt += "{\n"
+        for name, type in func_data['parameters'].items():
+            prompt += f"\"{name}\":"
+            prompt = self.model.decode(
+                self._get_argument_from_llm(type['type'], prompt))
+            prompt += "\n"
+
+        arguments = prompt.split("{\n")[-1]
+        print(arguments)
